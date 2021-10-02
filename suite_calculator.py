a801c0c404370c0978e0efb8a52916133a765d07
ZetaMap <56844734+ZetaMap@users.noreply.github.com>
2021-10-02 20:26:43 +0200

    Update suite_calculator.py

--- suite_calculator.py
+++ suite_calculator.py
@@ -1,6 +1,4 @@
-"""/!\\ Il est nécessaire de mettre la police Python à 'petit' pour un meilleur affichage. /!\\
-
-Ce programme permet de calculer la raison et u_0 d'une suite, arithmétique ou géométrique, à partir de 2 points donnés, 
+"""Ce programme permet de calculer la raison et u_0 d'une suite, arithmétique ou géométrique, à partir de 2 points donnés, 
 puis vous affiche le résultat proprement avec le calcul effectuer par la machine, pour vous aider.
 
 Le programme vous proposera ensuite,  si vous le voulez, de calculer des points spécifiques dans cette suite, 
