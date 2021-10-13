20adc3b734f454d4b26ed8da45c9628c376e2e1d
ZetaMap <56844734+ZetaMap@users.noreply.github.com>
2021-10-13 20:11:37 +0200

    Update suite_calculator.py

--- suite_calculator.py
+++ suite_calculator.py
@@ -12,19 +12,21 @@ def center(s, width, char=' '):
   if n%2 and width%2: half += 1
   return char*half + s + char*(n-half)
 
-a = input("1- Suite arthmétique \n2- Suite géométrique \n|-> ")
+###########################################
+
+a = input("1- Suite arthmétique \n2- Suite géométrique \n|-> ")
 while a != '1' and a != '2': a = input("|-> ")
-u, isnan, text = [[],[],["Départ :","Arrivé :"]], False, """
-Entrées :
-Départ : u_{} = {}
-Arrivé : u_{} = {}
+u, isnan, text = [[],[],["Départ :","Arrivé :"]], False, """
+Entrées :
+Départ : u_{} = {}
+Arrivé : u_{} = {}
 --------------------
 Calcul :{}
     {}
 {} = {} = {}
     {}
 --------------------
-Résultat :
+Résultat :
 u_n+1 = u_n{}
   u_0 = {}
 {}La suite est {}."""
@@ -43,7 +45,8 @@ if a == '1':
   except ZeroDivisionError: 
     pas = float("nan")
     isnan = True
-  u0, r, x, y = uu[0][1]-uu[0][0]*pas, str(round(pas, 7)), round(uu[0][1], 7), round(uu[1][1], 7)
+  u0, r, x, y = uu[0][1]-uu[0][0]*pas, str(round(pas, 5)), round(uu[0][1], 5), round(uu[1][1], 5)
+  if u0 == int(u0): u0 = int(u0)
   up, down = str(x)+'-'+('('+str(y)+')' if uu[1][1] < 0 else str(y)), str(uu[0][0])+'-'+('('+str(uu[1][0])+')' if uu[1][0] < 0 else str(uu[1][0]))
   b = max(len(up), len(down))
 
@@ -55,8 +58,8 @@ if a == '1':
     'r', '-'*b, r,
     center(down, b),
     r if pas < 0 else '+'+r,
-    round(u0, 7),
-    '\n', "nan" if isnan else "constante" if pas == 0 else "décroissante" if pas < 0 else "croissante"
+    round(u0, 5),
+    '\n',"nan" if isnan else "constante" if pas == 0 else "décroissante" if pas < 0 else "croissante"
   ))
   del uu, r, x, y, up, down, b 
 
@@ -65,7 +68,8 @@ else:
   except ZeroDivisionError: 
     pas = float("nan")
     isnan = True  
-  u0, r, d, x, y = u[0][1]/pas**u[0][0], str(round(pas, 7)), str(u[1][0]-u[0][0]), round(u[0][1], 7), round(u[1][1], 7)
+  u0, r, d, x, y = u[0][1]/pas**u[0][0], str(round(pas, 5)), str(u[1][0]-u[0][0]), round(u[0][1], 5), round(u[1][1], 5)
+  if u0 == int(u0): u0 = int(u0)
   b, e = max(len(str(x)), len(str(y))), len(d)
 
   print(text.format(
@@ -76,76 +80,84 @@ else:
     'q', d+" |"+'-'*b, r,
     ' '*e+"\\|"+center(str(x), b),
     '*'+('('+r+')' if pas < 0 else r),
-    round(u0, 7),
-    '', "nan" if isnan else "nulle" if pas == 0 else "constante" if pas == 1 else "décroissante" if pas < 0 else "croissante"
+    round(u0, 5),
+    '', "nan" if isnan else "nulle" if pas == 0 else "constante" if pas == 1 else "décroissante" if pas < 0 else "croissante"
   ))
   del d, x, y, b, e
 
 if not isnan:
   if input("Calculer des termes ? [o/n]: ") == 'o':
-    print("> N'écrivez rien pour quitter.\n")
+    print("> N'écrivez rien pour quitter.\n")
     t = input("u_")
     while t != '': 
-      try: print("u_"+t+" =", round(u0+pas*int(t) if a == "1" else u0*pas**int(t), 7))
+      try: print("u_"+t+" =", round(u0+pas*int(t) if a == "1" else u0*pas**int(t), 5))
       except OverflowError: print("u_"+t+" = inf")
       t = input("u_")
 
-  if input("Calculer la somme des termes ? [o/n]: ") == 'o':
-    print("> N'écrivez rien pour calculer la somme à \npartir des valeurs de la suite.\n")
-    s, text = [["Début :", "Fin :"]], """
-Entrées :
-Début : u_{} = {}
+  if input("Calculer la somme des termes ? \n[o/n]: ") == 'o':
+    print("> N'écrivez rien pour calculer \nla somme à partir des valeurs \nde la suite.\n")
+    s, text = [["Début :", "Fin :"]], """
+    Entrées :
+Début : u_{} = {}
 Fin : u_{} = {}
 --------------------
 Calcul :
     {}
-S = {} = {}
+S = {}
     {}
+  = {}
 --------------------
-Résultat :
+Résultat :
       {}
-S_n = {} = {}
-      {}"""
+S_n = {} 
+      {}
+    = {}"""
     try: 
       for i in range(2): 
         t = int(eval(input("{} u_".format(s[0][i]))))
-        s.append([t, u0+pas*t if a == '1' else u0*pas**t])
+        tt = u0+pas*t if a == '1' else u0*pas**t
+        if tt == int(tt): tt = int(tt)
+        s.append([t, tt])
     except (ValueError, SyntaxError): s += [u[0], u[1]]
-    del u
+    del u, t, tt, isnan
     s.pop(0)
     if s[0][0] > s[1][0]: s.reverse()
 
 
-    x, y, d = round(s[0][1], 7), round(s[1][1], 7), s[1][0]-s[0][0]
+    x, y, d = round(s[0][1], 5), round(s[1][1], 5), s[1][0]-s[0][0]
     if a == '1':
       up, m, down = str(x)+'+'+('('+str(y)+')' if s[1][1] < 0 else str(y)), '('+str(d)+"+1)x", None
-      b, bb, c = len(up), len(m), (d+1)*((s[0][1]+s[1][1])/2)
+      b, bb, c = len(up), len(m), round((d+1)*((s[0][1]+s[1][1])/2), 5)
       
       print(text.format(
         s[0][0], x,
         s[1][0], y,
         ' '*bb+up,
-        m+'-'*b, c,
+        m+'-'*b,
         ' '*bb+center(str(2), b),
+        c,
         "      u_i+u_n",
-        "(n+1)x-------", c,
-        "         2"
+        "(n+1)x-------", 
+        "         2",
+        c
       ))
 
     else:
-      up, m, down = "1-{}^({}+1)".format('('+r+')' if pas < 0 else r, d), str(u0)+'x', "1-"+('('+r+')' if pas < 0 else r)
-      b, bb, c = max(len(up), len(down)), len(m), u0*((1-pas**(d+1))/(1-pas))
+      up, m, down = "1-{}^({}+1)".format('('+r+')' if pas < 0 else r, d), str(round(u0, 5))+'x', "1-"+('('+r+')' if pas < 0 else r)
+      b, bb, c = max(len(up), len(down)), len(m), round(u0*((1-pas**(d+1))/(1-pas)), 5)
 
       print(text.format(
         s[0][0], x,
         s[1][0], y,
         ' '*bb+up,
-        m+'-'*b, c,
+        m+'-'*b,
        ' '*bb+center(down, b),
+       c,
        "    1-q^(n+1)",
-       "u_0x---------", c,
-       "       1-q"
+       "u_0x---------",
+       "       1-q",
+       c
       ))
 
-    del x, y, d, up, m, down, b, bb, c
+    del x, y, d, up, m, down, b, bb, c, s, u0, pas, text
 else: input()
